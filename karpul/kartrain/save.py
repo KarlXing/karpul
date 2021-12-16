6890eedf710d0742bfed103e6f9afca2814a536c
karlxing <jinweixing1006@gmail.com>
2021-12-15 22:01:09 -0800

    add save dicts

---
+++ karpul/kartrain/save.py
@@ -0,0 +1,17 @@
+# File: save.py
+# Author: Jinwei Xing (jinweixing1006@gmail.om)
+# Last Modified: Wednesday, 15th December 2021
+# This file is a part of karpul and distributed under MIT license.
+
+import json
+
+def save_dicts(path, dicts, indent=4):
+    '''
+    configs: a list of dicts to save
+    '''
+    with open(path, 'w') as f:
+        for dict in dicts:
+            json.dump(dict, f, indent=indent)
+            f.write('\n')
+
+
