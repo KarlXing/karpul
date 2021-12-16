e1f1333ac75b4810d592ba71e13a70a87f980342
karlxing <jinweixing1006@gmail.com>
2021-12-15 23:01:11 -0800

    save git

--- karpul/kartrain/save.py
+++ karpul/kartrain/save.py
@@ -5,15 +5,26 @@
 
 import json
 
-def save_dicts(path, dicts, indent=4):
+def save_workspace(path, dicts, git_path=None, indent=4):
     '''
     configs: a list of dicts to save
     '''
     with open(path, 'w') as f:
+        # save dicts
         if isinstance(dicts, list):
             for dict in dicts:
                 json.dump(dict, f, indent=indent)
                 f.write('\n')
         else:
             json.dump(dicts, f, indent=indent)
+
+        # save git
+        if git_path is not None:
+            try:
+                import git
+                repo = git.Repo.init(git_path)
+                commit = repo.head.commit.hexsha
+                f.write('\ngit commit: %s \n' % (commit))
+            except ImportError:
+                print('Save Workspace Failure: gitpython is not installed')
 
